504da1333587a1e0172231fc5b6a8bbd7efa8b93
Muzietto <marco_faustinelli@yahoo.com>
2017-05-04 21:28:24 +0200

    more functional idioms, but arrow syntax won't compile

--- chapter3/jsx/DateTimeNow.jsx
+++ chapter3/jsx/DateTimeNow.jsx
@@ -3,7 +3,13 @@ const React = require('react');
 class DateTimeNow extends React.Component {
   render() {
     let dateTimeNow = new Date().toLocaleString()
-    return <span>Current date and time for {this.props.userName} is {dateTimeNow}.</span>
+    return <div>{
+      function(uname) {
+        return (uname)
+          ?  <span>Current date and time for {uname} is {dateTimeNow}.</span>
+          :  <h1>Current date and time for ANONIMO is {dateTimeNow}.</h1>
+      }(this.props.userName)
+    }</div>
   }
 }
 
