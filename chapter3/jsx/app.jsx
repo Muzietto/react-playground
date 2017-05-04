b44ecd02267ff0f1dd614e3bdba4162f35427d3c
Muzietto <marco_faustinelli@yahoo.com>
2017-05-04 21:28:39 +0200

    composite elements

--- chapter3/jsx/app.jsx
+++ chapter3/jsx/app.jsx
@@ -1,9 +1,13 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const HelloWorld = require('./HelloWorld.jsx');
+const DateTimeNow = require('./DateTimeNow.jsx');
 
 ReactDOM.render((
-    <HelloWorld avverbio="cautiously"/>
+    <div>
+      <HelloWorld avverbio="cautiously"/>
+      <DateTimeNow/>
+    </div>
   ),
   document.getElementById('content')
 );
