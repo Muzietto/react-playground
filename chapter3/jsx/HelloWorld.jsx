821536be3d75ebdeb4880709d3cb3fd346905609
Muzietto <marco_faustinelli@yahoo.com>
2017-05-04 20:41:52 +0200

    starting to use local vars

--- chapter3/jsx/HelloWorld.jsx
+++ chapter3/jsx/HelloWorld.jsx
@@ -1,9 +1,12 @@
 const React = require('react');
 
+let helloWorldReactElement = <h1>Hello world!</h1>;
+
 class HelloWorld extends React.Component {
   render() {
     return (
       <div>
+        {helloWorldReactElement}
         <h1>1. Hello world!</h1>
         <h1>2. Hello world!</h1>
       </div>
