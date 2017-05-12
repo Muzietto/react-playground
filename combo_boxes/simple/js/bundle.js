c55033d3be1cc702589d065e8e6dcefcbbf89fb8
Muzietto <marco_faustinelli@yahoo.com>
2017-05-12 20:42:17 +0200

    first working simple combo box

---
+++ combo_boxes/simple/js/bundle.js
@@ -0,0 +1,101 @@
+var Dropdown = React.createClass({
+    displayName: 'Dropdown',
+
+
+    propTypes: {
+        id: React.PropTypes.string.isRequired,
+        options: React.PropTypes.array.isRequired,
+        value: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
+        valueField: React.PropTypes.string,
+        labelField: React.PropTypes.string,
+        onChange: React.PropTypes.func
+    },
+
+    getDefaultProps: function () {
+        return {
+            value: null,
+            valueField: 'value',
+            labelField: 'label',
+            onChange: null
+        };
+    },
+
+    getInitialState: function () {
+        var selected = this.getSelectedFromProps(this.props);
+        return {
+            selected: selected
+        };
+    },
+
+    componentWillReceiveProps: function (nextProps) {
+        var selected = this.getSelectedFromProps(nextProps);
+        this.setState({
+            selected: selected
+        });
+    },
+
+    getSelectedFromProps(props) {
+        var selected;
+        if (props.value === null && props.options.length !== 0) {
+            selected = props.options[0][props.valueField];
+        } else {
+            selected = props.value;
+        }
+        return selected;
+    },
+
+    render: function () {
+        var self = this;
+        var options = self.props.options.map(function (option) {
+            return React.createElement(
+                'option',
+                { key: option[self.props.valueField], value: option[self.props.valueField] },
+                option[self.props.labelField]
+            );
+        });
+        return React.createElement(
+            'select',
+            { id: this.props.id,
+                className: 'form-control',
+                value: this.state.selected,
+                onChange: this.handleChange },
+            options
+        );
+    },
+
+    handleChange: function (e) {
+        if (this.props.onChange) {
+            var change = {
+                oldValue: this.state.selected,
+                newValue: e.target.value
+            };
+            this.props.onChange(change);
+        }
+        this.setState({ selected: e.target.value });
+    }
+
+});
+var options = [{
+    description: 'This is option A',
+    code: 'a'
+}, {
+    description: 'This is option B',
+    code: 'b'
+}, {
+    description: 'This is option C',
+    code: 'c'
+}, {
+    description: 'This is option D',
+    code: 'd'
+}];
+
+var dropDownOnChange = function (change) {
+    alert('onChangeForSelect:\noldValue: ' + change.oldValue + '\nnewValue: ' + change.newValue);
+};
+
+ReactDOM.render(React.createElement(Dropdown, { id: 'myDropdown',
+    options: options,
+    value: 'b',
+    labelField: 'description',
+    valueField: 'code',
+    onChange: dropDownOnChange }), document.getElementById('container'));
---
+++ combo_boxes/simple/jsx/zapp.jsx
@@ -0,0 +1,35 @@
+var options = [
+    {
+        description: 'This is option A',
+        code: 'a'
+    },
+    {
+        description: 'This is option B',
+        code: 'b'
+    },
+    {
+        description: 'This is option C',
+        code: 'c'
+    },
+    {
+        description: 'This is option D',
+        code: 'd'
+    }
+];
+
+var dropDownOnChange = function(change) {
+    alert('onChangeForSelect:\noldValue: ' + 
+            change.oldValue + 
+            '\nnewValue: ' 
+            + change.newValue);
+};
+
+ReactDOM.render(
+    <Dropdown id='myDropdown' 
+              options={options} 
+              value='b'
+              labelField='description'
+              valueField='code'
+              onChange={dropDownOnChange}/>,
+    document.getElementById('container')
+);
