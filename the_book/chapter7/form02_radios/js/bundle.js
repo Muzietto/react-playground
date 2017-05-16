4af8d941d961f8069106748047d9de0e4d2e5b83
Muzietto <marco_faustinelli@yahoo.com>
2017-05-16 12:09:36 +0200

    stateless checkboxes and further more disappointing stateful form

--- the_book/chapter7/form02_radios/js/bundle.js
+++ the_book/chapter7/form02_radios/js/bundle.js
@@ -1,11 +1,19 @@
+let Checkbox02 = class Checkbox02 extends React.Component {
+  render() {
+    return React.createElement(
+      "label",
+      null,
+      React.createElement("input", {
+        type: "checkbox",
+        name: this.props.name,
+        value: this.props.value,
+        checked: this.props.checked,
+        onChange: this.props.handler }),
+      this.props.value
+    );
+  }
+};
 let Radio02 = class Radio02 extends React.Component {
-  constructor(params) {
-    super(params);
-    this.state = { title: 'Misster' };
-  }
-  handleChange(event) {
-    this.setState({ title: event.target.value });
-  }
   render() {
     return React.createElement(
       "label",
@@ -28,6 +36,12 @@ let Form02 = class Form02 extends React.Component {
         angular: false,
         react: true,
         muziettos: false
+      },
+      foods: {
+        pizza: false,
+        spaghetti: true,
+        steak: false,
+        cauliflower: false
       }
     };
   }
@@ -36,13 +50,23 @@ let Form02 = class Form02 extends React.Component {
     newStateMember[event.target.value] = true;
     this.setState({ frameworks: newStateMember });
   }
+  handleCheckbox(event) {
+    var newStateMember = Object.assign({}, this.state.foods);
+    newStateMember[event.target.value] = event.target.checked;
+    this.setState({ foods: newStateMember });
+  }
   render() {
     return React.createElement(
       "form",
       null,
       React.createElement(Radio02, { handler: this.handleRadio.bind(this), name: "frameworks", value: "angular", checked: this.state.frameworks.angular }),
       React.createElement(Radio02, { handler: this.handleRadio.bind(this), name: "frameworks", value: "react", checked: this.state.frameworks.react }),
-      React.createElement(Radio02, { handler: this.handleRadio.bind(this), name: "frameworks", value: "muziettos", checked: this.state.frameworks.muziettos })
+      React.createElement(Radio02, { handler: this.handleRadio.bind(this), name: "frameworks", value: "muziettos", checked: this.state.frameworks.muziettos }),
+      React.createElement("br", null),
+      React.createElement(Checkbox02, { handler: this.handleCheckbox.bind(this), name: "foods", value: "pizza", checked: this.state.foods['pizza'] }),
+      React.createElement(Checkbox02, { handler: this.handleCheckbox.bind(this), name: "foods", value: "spaghetti", checked: this.state.foods['spaghetti'] }),
+      React.createElement(Checkbox02, { handler: this.handleCheckbox.bind(this), name: "foods", value: "steak", checked: this.state.foods['steak'] }),
+      React.createElement(Checkbox02, { handler: this.handleCheckbox.bind(this), name: "foods", value: "cauliflower", checked: this.state.foods['cauliflower'] })
     );
   }
 };
--- the_book/chapter7/form02_radios/jsx/form02.jsx
+++ the_book/chapter7/form02_radios/jsx/form02.jsx
@@ -7,6 +7,12 @@ class Form02 extends React.Component {
         angular: false,
         react: true,
         muziettos: false,
+      },
+      foods: {
+        pizza: false,
+        spaghetti: true,
+        steak: false,
+        cauliflower: false,
       }
     };
   }
@@ -15,11 +21,21 @@ class Form02 extends React.Component {
     newStateMember[event.target.value] = true;
     this.setState({frameworks:newStateMember});
   }
+  handleCheckbox(event) {
+    var newStateMember = Object.assign({}, this.state.foods);
+    newStateMember[event.target.value] = event.target.checked;
+    this.setState({foods:newStateMember});
+  }
   render() {
   return <form>
     <Radio02 handler={this.handleRadio.bind(this)} name="frameworks" value="angular" checked={this.state.frameworks.angular}/>
     <Radio02 handler={this.handleRadio.bind(this)} name="frameworks" value="react" checked={this.state.frameworks.react}/>
     <Radio02 handler={this.handleRadio.bind(this)} name="frameworks" value="muziettos" checked={this.state.frameworks.muziettos}/>
+    <br/>
+    <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="pizza" checked={this.state.foods['pizza']}/>
+    <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="spaghetti" checked={this.state.foods['spaghetti']}/>
+    <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="steak" checked={this.state.foods['steak']}/>
+    <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="cauliflower" checked={this.state.foods['cauliflower']}/>
   </form>
   }
 }
