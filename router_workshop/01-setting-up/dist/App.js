de25443511b5b1583bcabbc13360b3b748e76164
Muzietto <marco_faustinelli@yahoo.com>
2017-05-16 21:09:12 +0200

    starting yet another react-router tutorial

---
+++ router_workshop/01-setting-up/dist/App.js
@@ -0,0 +1,26 @@
+define(['exports', 'react'], function (exports, _react) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _react2 = _interopRequireDefault(_react);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  exports.default = _react2.default.createClass({
+    displayName: 'App',
+    render: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        'Hello, React Router!'
+      );
+    }
+  });
+});
---
+++ router_workshop/01-setting-up/modules/start.js
@@ -0,0 +1,6 @@
+'use strict'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './App';
+
+ReactDOM.render(<Hello/>, document.getElementById('app'));
