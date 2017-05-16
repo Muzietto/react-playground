7dba71f782de82b84216ff98a372d6e3402efa0d
Muzietto <marco_faustinelli@yahoo.com>
2017-05-16 12:22:59 +0200

    form handles selects

--- the_book/chapter7/form02_radios/jsx/form02.jsx
+++ the_book/chapter7/form02_radios/jsx/form02.jsx
@@ -26,6 +26,9 @@ class Form02 extends React.Component {
     newStateMember[event.target.value] = event.target.checked;
     this.setState({foods:newStateMember});
   }
+  handleSelect(event) {
+    this.setState({selectedLanguage:event.target.value});
+  }
   render() {
   return <form>
     <Radio02 handler={this.handleRadio.bind(this)} name="frameworks" value="angular" checked={this.state.frameworks.angular}/>
@@ -36,6 +39,14 @@ class Form02 extends React.Component {
     <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="spaghetti" checked={this.state.foods['spaghetti']}/>
     <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="steak" checked={this.state.foods['steak']}/>
     <Checkbox02 handler={this.handleCheckbox.bind(this)} name="foods" value="cauliflower" checked={this.state.foods['cauliflower']}/>
+    <br/>
+    <select name="language" value={this.state.selectedValue}
+      onChange={this.handleSelect.bind(this)}>
+      <option value="ruby">Ruby</option>
+      <option value="node">Node.js</option>
+      <option value="java">Java</option>
+      <option value="python">Python</option>
+    </select>
   </form>
   }
 }
