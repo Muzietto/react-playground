679d18a78615fb38e3a7bc52db3da7acd8835171
Muzietto <marco_faustinelli@yahoo.com>
2017-05-20 11:55:36 +0200

    list maps items

--- combo_boxes/simple/jsx/itemsList.jsx
+++ combo_boxes/simple/jsx/itemsList.jsx
@@ -3,15 +3,14 @@ var ItemsList = React.createClass({
   propTypes: {
     items: React.PropTypes.arrayOf(React.PropTypes.shape({
       id: React.PropTypes.number.isRequired,
-      //name: React.PropTypes.string.isRequired,
     }).isRequired).isRequired,
-    //onItemClick: React.PropTypes.func.isRequired
+    mapper: React.PropTypes.func.isRequired
   },
   render: function() {
     var self = this; 
     var items = this.props.items.sort((a,b) => a.id-b.id).map(function(item) {
       return <li key={item.id}>
-        {item.id},&nbsp;{item.name}
+        {self.props.mapper(item)}
       </li>
     });
 
