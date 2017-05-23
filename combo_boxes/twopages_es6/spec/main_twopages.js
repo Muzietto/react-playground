1c4319dc4df7bc374caf06cfca66172a09e917fe
Muzietto <marco_faustinelli@yahoo.com>
2017-05-23 21:33:24 +0200

    load requires, but not imports

--- combo_boxes/twopages_es6/spec/main_twopages.js
+++ combo_boxes/twopages_es6/spec/main_twopages.js
@@ -1,13 +1,13 @@
 (function() {
 
   require.config({
-    baseUrl: 'twopages_es6/dist/reducers',
+    baseUrl: './twopages_es6',
     paths: {
-      'reducer': 'mainReducer',
-      //'actions': './actions/mainReducer',
-      'twopages_test': '../../spec/twopages_test',
-      'mocha': '../../spec/mocha/mocha',
-      'chai': '../../spec/mocha/chai',
+      'reducer': './dist/reducers/mainReducer',
+      'actions': './dist/actions/actions',
+      'twopages_test': './spec/twopages_test',
+      'mocha': './spec/mocha/mocha',
+      'chai': './spec/mocha/chai',
     },
     shim: {
       mocha: {
--- combo_boxes/twopages_es6/spec/twopages_test.js
+++ combo_boxes/twopages_es6/spec/twopages_test.js
@@ -1,11 +1,20 @@
 (function() {
   'use strict';
 
-  define(['reducer', 'chai'], function(reducer, chai) {
-    var expect  = chai.expect;
+  define([
+      'reducer',
+      'actions',
+      'chai'
+    ], function(defaultReducer, actions, chai) {
+    var expect  = chai.expect,
+    reducer = defaultReducer.mainReducer,
+    ActionTypes = actions.ActionTypes
+    ActionCreators = actions.ActionCreators
+    ;
 
     describe('the main reducer', function() {
       it('can alert the user without modifying state', function() {
+        var alert = x => x;
         var state = {test:12};
         var action = {
           type: 'ALERT_USER',
