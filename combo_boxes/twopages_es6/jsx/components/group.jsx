ab0ebae15607ade0a45d5ca819e980279cf1e010
Muzietto <marco_faustinelli@yahoo.com>
2017-05-25 12:18:55 +0200

    better view for single user/group

--- combo_boxes/twopages_es6/jsx/components/group.jsx
+++ combo_boxes/twopages_es6/jsx/components/group.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 class Group extends React.Component {
   render() {
     return <span id={'groupDiv' + this.props.data.id}>
-      <h3>Details group</h3>
+      <span className="separated-span">Details group</span>
       <span className="separated-span">{this.props.data.id}</span>
       <span className="separated-span">{this.props.data.name}</span>
     </span>
--- combo_boxes/twopages_es6/jsx/components/user.jsx
+++ combo_boxes/twopages_es6/jsx/components/user.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 class User extends React.Component {
   render() {
     return <span id={'userDiv' + this.props.data.id}>
-      <h3>Details user</h3>
+      <span className="separated-span">Details user</span>
       <span className="separated-span">{this.props.data.id}</span>
       <span className="separated-span">{this.props.data.name}</span>
     </span>
