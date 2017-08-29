3123e60085916a3a8717bc0bb2d12f5fdd5d40a7
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-08-29 17:33:30 +0200

    further logging to verify things

--- the_book/string_wizard/jsx/app.jsx
+++ the_book/string_wizard/jsx/app.jsx
@@ -3,7 +3,9 @@ import wrapper from 'input_wrapper.es6';
 
 function wrappingHandler(ev) {
     var wrapped = wrapper(ev.target);
+    console.log(wrapped.read());
     wrapped.write('changed_text');
+    console.log(wrapped.read());
 }
 
 document
