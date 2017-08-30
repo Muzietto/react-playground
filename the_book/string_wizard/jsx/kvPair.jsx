75270cc9043d8b69e409d0901a18a7150cfc18f4
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-08-30 12:49:13 +0200

    first implementation of kvPair

--- the_book/string_wizard/jsx/kvPair.jsx
+++ the_book/string_wizard/jsx/kvPair.jsx
@@ -3,14 +3,24 @@
 
 import React from 'react';
 
-class User extends React.Component {
-  render() {
-    return <div id="kvPairDiv{this.props.data.id}">
-      <span className="separated-span">{this.props.data.id}</span>
-      <span className="separated-span">{this.props.data.name}</span>
-      <a onClick={this.props.callbacks.delete}>REMOVE</a>
-    </div>
-  }
+class KvPair extends React.Component {
+    render() {
+        return <div className="kvpair">
+            <input
+                id={'kvpair_' + this.props.data.id + '_key'}
+                className="kvpair__input kvpair__input--key"
+                value={this.props.data.key}/>
+            <label className="label kvpair__label--separator">:</label>
+            <input
+                id="kvpair0value"
+                className="kvpair__input kvpair__input--value"
+                value={this.props.data.value}/>
+            <button
+                onClick={this.props.callbacks.delete}
+                className="button kvpair__button">DEL
+            </button>
+        </div>;
+    }
 }
 
-export default User;
+export default KvPair;
