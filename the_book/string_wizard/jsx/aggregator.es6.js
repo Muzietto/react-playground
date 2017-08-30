bfb2899c1d52d7fe0a4958182fb31b6b4f96e6ab
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-08-30 09:51:30 +0200

    aggregator encodes and decodes

--- the_book/string_wizard/jsx/aggregator.es6.js
+++ the_book/string_wizard/jsx/aggregator.es6.js
@@ -1,18 +1,23 @@
 'use strict';
 
 function aggregator() {
+
     var _compose = (baseUrl, kvList) => kvList
+        .map(kv => [kv[0], encodeURIComponent(kv[1])])
         .reduce((acc, kvPair) => acc + kvPair
             .join('=') + '&', baseUrl + '?')
         .slice(0, -1); // remove last &
+
     var _decompose = url => url
         .split('?')[1]
         .split('&')
-        .map(str => str.split('='));
+        .map(str => str.split('='))
+        .map(kv => [kv[0], decodeURIComponent(kv[1])]);
+
     return {
         compose: _compose,
         decompose: _decompose,
-    }
+    };
 }
 
 export default aggregator();
