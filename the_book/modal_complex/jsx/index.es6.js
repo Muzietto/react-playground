f0ec249d481741a543a6864313bcf8727d9ec422
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-08-30 21:05:41 +0200

    complex modal stateful example

---
+++ the_book/modal_complex/jsx/index.es6.js
@@ -0,0 +1,6 @@
+import React from 'lib/react';
+import ReactDOM from 'lib/react-dom';
+//import './index.css';
+import App from 'app.es6';
+
+ReactDOM.render(<App />, document.getElementById('root'));
