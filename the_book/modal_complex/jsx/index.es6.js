5cfead35721012f50725fefeaa5e0d8d002050c6
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-08-30 21:17:25 +0200

    getting rid of css cruft

--- the_book/modal_complex/jsx/index.es6.js
+++ the_book/modal_complex/jsx/index.es6.js
@@ -1,6 +1,7 @@
 import React from 'lib/react';
 import ReactDOM from 'lib/react-dom';
-//import './index.css';
 import App from 'app.es6';
+
+App.membroStatico();
 
 ReactDOM.render(<App />, document.getElementById('root'));
