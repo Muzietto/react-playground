124d6ebe0d398da07fc39690d4a653b064e25556
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2017-10-19 15:30:44 +0200

    starting complex_templates state machine

---
+++ b_stateMachine/complex_templates/jsx/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import {startStep} from './state_machine/steps';
+
+startStep();
---
+++ b_stateMachine/complex_templates/jsx/state_machine/view.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('container');
+
+export default function choice(choices, template) {
+    template = template || templateA;
+
+    _render(choices, template);
+}
+
+function _render(handlers, template) {
+    ReactDOM.render(template(handlers), root);
+}
+
+export function templateA(handlers) {
+    return _fieldset(handlers, 'A');
+}
+
+export function templateB(handlers) {
+    return _fieldset(handlers, 'B');
+}
+
+export function templateC(handlers) {
+    return _fieldset(handlers, 'C');
+}
+
+function _fieldset(handlers, letter) {
+    return (
+        <fieldset>
+            <legend>{letter + ') This is the ' + handlers.location}</legend>
+            {
+                handlers.backward.map((handler, index) => {
+                    return <h4
+                        style={{cursor: 'pointer'}}
+                        key={index}
+                        onClick={handler}>{'back to ' + (handler.name || 'noname')}</h4>;
+                })
+            }
+            {
+                handlers.forward.map((handler, index) => {
+                    return <h2
+                        style={{cursor: 'pointer'}}
+                        key={index}
+                        onClick={handler}><p>forward to&nbsp;&nbsp;</p>{(handler.name || 'noname')}</h2>;
+                })
+            }
+        </fieldset>
+    );
+}
