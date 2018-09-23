f587aa4aa6eb9c770be0ea549560153b154fc661
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-23 09:35:35 +0200

    introducing history

---
+++ router_homemade/first_example/App.js
@@ -0,0 +1,7 @@
+import history from './history';
+
+function render(location) { /* Render React app, read on */ }
+
+render(history.getCurrentLocation()); // render the current URL
+
+history.listen(render);               // render subsequent URLs
