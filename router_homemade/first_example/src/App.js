ef9737f1628dc050e9155b50e5d0ee1a7c318ef4
Marco Faustinelli <marco_faustinelli@yahoo.com>
2018-09-24 16:53:25 +0200

    no gulp for now + minor changes

--- router_homemade/first_example/src/App.js
+++ router_homemade/first_example/src/App.js
@@ -5,8 +5,8 @@ class App extends React.Component {
   transition = event => {
     event.preventDefault();
     history.push({
-      pathname: event.currentTarget.pathname,
-      search: event.currentTarget.search
+      pathname: event.currentTarget.pathname, // path
+      search: event.currentTarget.search // querystring
     });
   };
   render() {
@@ -15,6 +15,7 @@ class App extends React.Component {
         <li><a href="/" onClick={this.transition}>Home</a></li>
         <li><a href="/one" onClick={this.transition}>One</a></li>
         <li><a href="/two" onClick={this.transition}>Two</a></li>
+        <li><a href="/two?q=12" onClick={this.transition}>Two q=12</a></li>
       </ul>
     );
   }
