fce118094b6a1859ed3e0547335809a10dde023b
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-25 06:22:50 +0200

    understanding it's not a second App...

--- router_homemade/first_example/src/App2.js
+++
@@ -1,7 +0,0 @@
-import history from './history';
-
-function render(location) { /* Render React app, read on */ }
-
-render(history.getCurrentLocation()); // render the current URL
-
-history.listen(render);               // render subsequent URLs
