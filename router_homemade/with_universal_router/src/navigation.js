d89f26f921b18badd4d09507eafb7974216217a0
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-26 19:38:17 +0200

    my example fully working!

--- router_homemade/with_universal_router/src/navigation.js
+++ router_homemade/with_universal_router/src/navigation.js
@@ -2,21 +2,16 @@ import UniversalRouter from 'universal-router'
 import ReactDOM from 'react-dom';
 import routes from './routes';
 
-console.log('routes=' + JSON.stringify(routes))
-
 const router = new UniversalRouter(routes)
 const container = document.getElementById('container');
 
 export default function navigateTo(location) {
-  try {
-
-  router.resolve(location)
-    .then(renderComponent)
-    .catch(function(error) {
-      router.resolve({ pathname: '/error', error })
-        .then(renderComponent)
-    });
-  } catch (e) {}
+    router.resolve(location)
+      .then(renderComponent)
+      .catch(function(error) {
+        router.resolve({ pathname: '/error', error })
+          .then(renderComponent)
+      });
 }
 
 function renderComponent(component) {
