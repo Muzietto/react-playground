7f9d61bc471c977b12e4c556e4cecac6c23da3da
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-27 05:50:07 +0200

    navigation modifies history

--- router_homemade/with_universal_router/src/navigation.js
+++ router_homemade/with_universal_router/src/navigation.js
@@ -1,11 +1,13 @@
 import UniversalRouter from 'universal-router'
 import ReactDOM from 'react-dom';
 import routes from './routes';
+import history from './history';
 
 const router = new UniversalRouter(routes)
 const container = document.getElementById('container');
 
 export default function navigateTo(location) {
+    history.push(location);
     router.resolve(location)
       .then(renderComponent)
       .catch(function(error) {
