0b053487da5970a62e9e7ead215637830d90a67b
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-08-08 15:03:20 +0200

    finalised FillingSpace (justify-content)

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import UnderstandingBreakpoints from '@src/cookbook/5-grid/UnderstandingBreakpoints';
+import FillingSpace from '@src/cookbook/5-grid/FillingSpace';
 
 export default function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <h1>Hello World!!!</h1>
+      <FillingSpace justify='space-around' />
       <UnderstandingBreakpoints />
     </React.Fragment>
   );
