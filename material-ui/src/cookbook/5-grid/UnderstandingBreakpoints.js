a8a2d6294c383b9c28739aea7bb9fdb234464da6
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-08-08 15:02:48 +0200

    finalised UnderstandingBreakpoints

--- material-ui/src/cookbook/5-grid/UnderstandingBreakpoints.js
+++ material-ui/src/cookbook/5-grid/UnderstandingBreakpoints.js
@@ -17,17 +17,47 @@ const styles = theme => ({
 const UnderstandingBreakpoints = withStyles(styles)(({ classes }) => (
   <div className={classes.root}>
     <Grid container spacing={4}>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=4</Paper>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=4</Paper>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=4</Paper>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=4</Paper>
+      </Grid>
+    </Grid>
+    <hr />
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={9} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=9 md=4</Paper>
+      </Grid>
+      <Grid item xs={12} sm={9} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=9 md=4</Paper>
+      </Grid>
+      <Grid item xs={12} sm={9} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=9 md=4</Paper>
+      </Grid>
+      <Grid item xs={12} sm={9} md={4}>
+        <Paper className={classes.paper}>xs=12 sm=9 md=4</Paper>
+      </Grid>
+    </Grid>
+    <hr />
+    <Grid container spacing={4}>
+      <Grid item xs='auto' sm='auto' md='auto'>
+        <Paper className={classes.paper}>xs=auto sm=auto md=auto</Paper>
+      </Grid>
+      <Grid item xs='auto' sm='auto' md='auto'>
+        <Paper className={classes.paper}>xs=auto sm=auto md=auto</Paper>
+      </Grid>
+      <Grid item xs='auto' sm='auto' md='auto'>
+        <Paper className={classes.paper}>xs=auto sm=auto md=auto</Paper>
+      </Grid>
+      <Grid item xs='auto' sm='auto' md='auto'>
+        <Paper className={classes.paper}>xs=auto sm=auto md=auto</Paper>
       </Grid>
     </Grid>
   </div>
