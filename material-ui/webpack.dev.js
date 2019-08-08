d95fde29fc4b108ac5493865a554957b4364b214
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-08-08 14:08:09 +0200

    aliasing in webpack

--- material-ui/webpack.dev.js
+++ material-ui/webpack.dev.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
