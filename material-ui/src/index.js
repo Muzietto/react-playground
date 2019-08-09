b3962088310d7cbe87ab0ceb7b22d79f5a757477
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-08-09 08:41:33 +0200

    AppBar fixed position

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import UnderstandingBreakpoints from '@src/cookbook/5-grid/UnderstandingBreakpoints';
-import FillingSpace from '@src/cookbook/5-grid/FillingSpace';
-import FixedColumnLayout from '@src/cookbook/5-grid/FixedColumnLayout';
-import ColumnDirection from '@src/cookbook/5-grid/ColumnDirection';
+// import UnderstandingBreakpoints from '@src/cookbook/5-grid/UnderstandingBreakpoints';
+// import FillingSpace from '@src/cookbook/5-grid/FillingSpace';
+// import FixedColumnLayout from '@src/cookbook/5-grid/FixedColumnLayout';
+// import ColumnDirection from '@src/cookbook/5-grid/ColumnDirection';
+import FixedPosition from '@src/cookbook/2-appbar/FixedPosition';
 
 export default function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <h3>ColumnDirection</h3>
-      <ColumnDirection />
-      <hr />
-      <h3>FixedColumnLayout</h3>
-      <h5>Only xs (&gt; 0px) is specified.</h5>
-      <h5>4 cols (xs=3/12)</h5>
-      <FixedColumnLayout width={3} />
-      <h5>2 cols (xs=6/12)</h5>
-      <FixedColumnLayout width={6} />
-      <h5>Header one col - rest is 2 cols</h5>
-      <FixedColumnLayout width={6} singleHeader={true} />
-      <hr />
-      <h3>FillingSpace</h3>
-      <FillingSpace justify='space-around' />
-      <hr />
-      <h3>UnderstandingBreakpoints</h3>
-      <UnderstandingBreakpoints />
+      <div>
+        <h3>FixedPosition</h3>
+        <FixedPosition />
+        <hr />
+      </div>
+      {/* <div>
+        <h3>ColumnDirection</h3>
+        <ColumnDirection />
+        <hr />
+        <h3>FixedColumnLayout</h3>
+        <h5>Only xs (&gt; 0px) is specified.</h5>
+        <h5>4 cols (xs=3/12)</h5>
+        <FixedColumnLayout width={3} />
+        <h5>2 cols (xs=6/12)</h5>
+        <FixedColumnLayout width={6} />
+        <h5>Header one col - rest is 2 cols</h5>
+        <FixedColumnLayout width={6} singleHeader={true} />
+        <hr />
+        <h3>FillingSpace</h3>
+        <FillingSpace justify='space-around' />
+        <hr />
+        <h3>UnderstandingBreakpoints</h3>
+        <UnderstandingBreakpoints />
+      </div> */}
     </React.Fragment>
   );
 }
