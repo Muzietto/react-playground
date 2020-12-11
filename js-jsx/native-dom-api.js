8304d31ff1221374dc636acb164f35a9982990b3
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2020-12-11 12:48:38 +0100

    js-jsx: first clickable index.html

---
+++ js-jsx/native-dom-api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ce = document.createElement;
+const eid = document.getElementById;
+
+
+
+
+// var paragrafo = document.createElement('p');
+// paragrafo.style.textDecoration = 'underline';
+// paragrafo.style.color = 'blue';
+// paragrafo.style.marginLeft = '30px';
+// paragrafo.innerText = 'il mattino ha l\'oro in bocca';
+// var palla = document.getElementById('palla');
+// palla.appendChild(paragrafo);
