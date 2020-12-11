487c0abf2d3aaef4358ac145b0466feadf2d7d72
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2020-12-11 13:12:48 +0100

    js-jsx: procedural DOM builder ready

--- js-jsx/native-dom-api.js
+++ js-jsx/native-dom-api.js
@@ -3,7 +3,36 @@
 const ce = document.createElement;
 const eid = document.getElementById;
 
+function procedural(struct, parent) {
+  const {
+    type,
+    id,
+    style,
+    innerText,
+    children,
+  } = struct;
 
+  const newElement = document.createElement(type);
+
+  newElement.id = id;
+
+  if (innerText) newElement.innerText = innerText;
+
+  if (style) {
+    Object.keys(style)
+      .forEach(attribute => {
+        newElement.style[attribute] = style[attribute];
+      });
+  }
+
+  if (children) {
+    children.forEach(child => {
+      procedural(child, newElement);
+    });
+  }
+
+  parent.appendChild(newElement);
+}
 
 
 // var paragrafo = document.createElement('p');
