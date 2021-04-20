f6cb6425a8f52efb828f180f4c3e2602f88e4c12
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2021-04-20 16:49:20 +0200

    React integrated with Folktale

--- folktale-react/src/index.js
+++ folktale-react/src/index.js
@@ -1,15 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Maybe from 'folktale/maybe';
 import compose from 'folktale/core/lambda/compose';
 
-const inc = x => x + 1;
-const double = x => x * 2;
+ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log(Maybe.Just(1).map(compose(double, inc)));
+function App() {
 
-const n = 5;
-const result = n
-  |> double
-  |> double
-  |> (x => { console.log(x); return x + 1; })
-  |> double;
-console.log(result);
+  const inc = x => x + 1;
+  const double = x => x * 2;
+
+  const theMaybe = Maybe.Just(1).map(compose(double, inc));
+
+  const n = 5;
+  const result = n
+    |> double
+    |> double
+    |> (x => { console.log(x); return x + 1; })
+    |> double;
+
+  return <div>
+    {`theMaybe=${theMaybe}`}
+    <hr />
+    {`result=${result}`}
+  </div>;
+}
