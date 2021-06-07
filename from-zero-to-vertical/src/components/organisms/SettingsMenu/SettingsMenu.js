5cf2f72b4b19f650f935647e85ee94a18937f6cb
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2021-06-07 17:19:53 +0200

    00-wireframe: completed preparation of TopNavigation

--- from-zero-to-vertical/src/components/organisms/SettingsMenu/SettingsMenu.js
+++ from-zero-to-vertical/src/components/organisms/SettingsMenu/SettingsMenu.js
@@ -10,67 +10,45 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default function SettingsMenu({ children = '' }) {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
+
+  const open = true;
+
   const anchorRef = useRef(null);
-
-  const handleToggle = () => {
-    setOpen(prevOpen => !prevOpen);
-  };
-
-  const handleClose = event => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
-  useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
 
   return (
     <div className={classes.root}>
       <div>
         <Button
           ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
-          aria-haspopup='true'
-          onClick={handleToggle}
           className={classes.button}
         >
           Impostazioni
         </Button>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal className={classes.popper}>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+          className={classes.popper}
+        >
           {({ TransitionProps, placement }) => (
             <Grow
               {...TransitionProps}
               style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
             >
               <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id='menu-list-grow' onKeyDown={handleListKeyDown}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id='menu-list-grow'
+                  >
                     {children &&
                         React.Children.map(children, child => (
-                          <MenuItem onClick={handleClose}>
+                          <MenuItem onClick={() => {}}>
                             {React.cloneElement(child)}
                           </MenuItem>
                         ))}
                   </MenuList>
-                </ClickAwayListener>
               </Paper>
             </Grow>)}
         </Popper>
